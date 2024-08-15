b5477d4a24ac3e71edfc0b78e2a547ff46f8ac81
manlio-it <italylovechess@gmail.com>
2024-08-15 22:57:14 +0200

    Final

--- app/src/main/java/com/example/salutemendietamediterranea/SaluteMenuPrincipale.kt
+++ app/src/main/java/com/example/salutemendietamediterranea/SaluteMenuPrincipale.kt
@@ -3,7 +3,6 @@ package com.example.salutemendietamediterranea
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 
 class SaluteMenuPrincipale : AppCompatActivity() {
--- app/src/main/java/com/example/salutemendietamediterranea/Super_Food.kt
+++ app/src/main/java/com/example/salutemendietamediterranea/Super_Food.kt
@@ -1,6 +1,5 @@
 package com.example.salutemendietamediterranea
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
